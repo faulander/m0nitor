063930e971d99831663d1d033af8f6ad56e89656
Harald Fauland <harald.fauland@gmail.com>
2019-09-15 16:07:54 +0200

    flask up and running

--- m0nitor.py
+++ m0nitor.py
@@ -1,20 +1,31 @@
 import yaml
+import json
 from loguru import logger
 from config import load_config
 from sonarr import Sonarr
 from elk import ELK
+from flask import Flask, render_template
+
 
 config = load_config("m0nitor.yml")
-print(config)
+content = dict()
+#print(config)
 s = Sonarr(config['sonarr']['URL'], config['sonarr']['API_KEY'])
 e = ELK(config['elk']['URL'], config['elk']['PORT'])
 services = config['elk']['SERVICES']
-logger.debug("Configured services: {}", services)
+#logger.debug("Configured services: {}", services)
 for service in services:
     logs = e.getLogs(service)
-    for log in logs:
-        logger.debug("Docker - {} - {}", service, log)
-for episode in s.getLastEpisodes():
-        logger.debug("Sonarr - {}", episode)
-for pfad in s.getDiskSpace():
-        logger.debug("Sonarr - {}", pfad)
+    content[service] = logs
+
+#logger.debug("Sonarr - {}", episode)
+content['sonarr'] = s.getLastEpisodes()
+content['diskspace'] = s.getDiskSpace()
+logger.debug(content)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('m0nitor.html', data=content)
